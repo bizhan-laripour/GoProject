432524b433910a6b251493a24ec0bf0fe760d757
bizhan <bizhan.laripour.herat@gmail.com>
2024-02-17 09:12:17 -0500

    variables and dataTypes in golang

--- main.go
+++ main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// creating basic variable
+	var example = "hello"
+	fmt.Println("hello ", example)
+	//creating variables with Type
+	var name string = "bizhan"
+	fmt.Println(name)
+
+	//creating variable and assign value to it
+	username := "bizhan.lp"
+	fmt.Println(username)
+
+	//get default value of a numeric variable
+	var sum int
+	fmt.Println(sum)
+	//get default value of a String value
+	var defaultOfString string
+	fmt.Println(defaultOfString)
+
+	//compound value assignment
+	part1, part2 := 1, 2
+	fmt.Println(part1, part2)
+	sum = part1 + part2
+	fmt.Println(sum)
+
+	//multiple variables
+	var (
+		lastName  = "laripour"
+		className = "Golang"
+	)
+	fmt.Println(lastName, className)
 }
