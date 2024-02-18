d6139a1495c20a9dbfd49ded15344e44474cea6f
bizhan <bizhan.laripour.herat@gmail.com>
2024-02-18 10:22:53 -0500

    Struct in golang

--- structure/main.go
+++ structure/main.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// use of a simple struct
 	data := Sample{
 		name:     "bizhan",
 		lastName: "laripour",
 		age:      32,
 	}
+	data.age = 25
+	fmt.Println(data.age)
 	fmt.Println(data.name)
+
+	//use of anonymous and inline anonymous struct
+	anonymous()
+	inlineAnonymous()
 
 }
--- structure/structure.go
+++ structure/structure.go
@@ -1,7 +1,51 @@
 package main
 
+import "fmt"
+
+/*
+*
+This is a struct like a class in java
+*/
 type Sample struct {
 	name     string
 	lastName string
 	age      int
+}
+
+/*
+*
+this is anonymous type that used in a function
+*/
+func anonymous() {
+
+	var anonymousType struct {
+		name     string
+		lastName string
+		age      int
+	}
+	anonymousType.age = 10
+	anonymousType.name = "anonymous"
+	anonymousType.lastName = "lastName"
+	fmt.Println(anonymousType.lastName)
+	fmt.Println(anonymousType.name)
+	fmt.Println(anonymousType.age)
+}
+
+/*
+*
+this is inline anonymous type that used in a function
+*/
+func inlineAnonymous() {
+	inline := struct {
+		name     string
+		lastName string
+		age      int
+	}{
+		"Inline anonymous name",
+		"Inline anonymous lastName",
+		5,
+	}
+	fmt.Println(inline.age)
+	fmt.Println(inline.name)
+	fmt.Println(inline.lastName)
 }
