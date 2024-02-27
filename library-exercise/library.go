84bd4a407468f9a63073d08629a9944a12666926
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-02-27 16:59:50 +0330

    Update library.go

--- library-exercise/library.go
+++ library-exercise/library.go
@@ -28,26 +28,39 @@ var books []Book
 var users []User
 
 func main() {
-    
+
     // create user
     user := User{
        name:     "bizhan",
        lastName: "laripour",
        id:       1,
     }
+    user2 := User{
+       name:     "ali",
+       lastName: "ahmadi",
+       id:       2,
+    }
     //register user
     registerUserToLibrary(user)
-    
+    registerUserToLibrary(user2)
+
     // create book
     book := Book{
-       name: "golang",
-       ISBN: "123654789",
+       name:     "golang",
+       ISBN:     "123654789",
        isBooked: false,
     }
-    
+    book2 := Book{
+       name:     "java",
+       ISBN:     "987654321",
+       isBooked: false,
+    }
+
     // add book to library
     addBookToLibrary(book)
-    bookABook(book , user);
+    addBookToLibrary(book2)
+    bookABook(book2, user)
+    bookABook(book, user2)
     fmt.Println(library)
 }
 
@@ -69,14 +82,22 @@ func registerUserToLibrary(user User) {
     library.users = append(library.users, user)
 }
 
-func bookABook(book Book , user User)  {
+func bookABook(book Book, user User) {
     book.isBooked = true
     book.bookedBy = user.id
-    user.book = append(user.book , book)
-    for _ , value := range library.bookSlice{
-       if(value.ISBN == book.ISBN){
+    user.book = append(user.book, book)
+    for id, value := range library.bookSlice {
+       if value.ISBN == book.ISBN {
           value.isBooked = true
           value.bookedBy = user.id
+          library.bookSlice[id] = value
        }
+    }
+    for id, value := range library.users {
+       if value.id == user.id {
+          value.book = append(value.book, book)
+          library.users[id] = value
+       }
+
     }
 }
