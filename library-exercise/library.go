fb335f20fdadd6664f790dc7812adfed3d5f0753
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-02-27 16:23:40 +0330

    Update library.go

--- library-exercise/library.go
+++ library-exercise/library.go
@@ -1,4 +1,19 @@
+package main
 
+import "fmt"
+
+type Book struct {
+    name     string
+    ISBN     string
+    isBooked bool
+    bookedBy int
+}
+
+type User struct {
+    id       int
+    name     string
+    lastName string
+    book     []Book
 }
 
 type Library struct {
@@ -13,7 +28,27 @@ var books []Book
 var users []User
 
 func main() {
-
+    
+    // create user
+    user := User{
+       name:     "bizhan",
+       lastName: "laripour",
+       id:       1,
+    }
+    //register user
+    registerUserToLibrary(user)
+    
+    // create book
+    book := Book{
+       name: "golang",
+       ISBN: "123654789",
+       isBooked: false,
+    }
+    
+    // add book to library
+    addBookToLibrary(book)
+    bookABook(book , user);
+    fmt.Println(library)
 }
 
 func addUserToUserSlice(user User) []User {
@@ -32,4 +67,16 @@ func addBookToLibrary(book Book) {
 
 func registerUserToLibrary(user User) {
     library.users = append(library.users, user)
+}
+
+func bookABook(book Book , user User)  {
+    book.isBooked = true
+    book.bookedBy = user.id
+    user.book = append(user.book , book)
+    for _ , value := range library.bookSlice{
+       if(value.ISBN == book.ISBN){
+          value.isBooked = true
+          value.bookedBy = user.id
+       }
+    }
 }
