841a369b171a62fd26f897f9443757c1670344d7
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-02-27 07:34:15 -0500

    Update library.go

--- library-exercise/library.go
+++ library-exercise/library.go
@@ -1,1 +1,35 @@
-package library_exercise
+
+}
+
+type Library struct {
+    name      string
+    bookSlice []Book
+    users     []User
+}
+
+var library Library
+var books []Book
+
+var users []User
+
+func main() {
+
+}
+
+func addUserToUserSlice(user User) []User {
+    users = append(users, user)
+    return users
+}
+
+func addBookToBookSlice(book Book) []Book {
+    books = append(books, book)
+    return books
+}
+
+func addBookToLibrary(book Book) {
+    library.bookSlice = append(library.bookSlice, book)
+}
+
+func registerUserToLibrary(user User) {
+    library.users = append(library.users, user)
+}
